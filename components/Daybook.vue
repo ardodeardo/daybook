1fcc15db52aaba6a179c5610f138979ac2de75e6
ardodeardo-mytpgy <deardo.dibrianto@mytripology.com>
2023-11-16 11:23:51 +0700

    feat: remove refresh button. update button style

--- components/Daybook.vue
+++ components/Daybook.vue
@@ -181,7 +181,7 @@ export default {
           class="flex flex-auto flex-col justify-center items-center p-4 md:p-5"
         >
           <img src="/oc-collection.png" alt="" class="mx-auto w-[100px]" />
-          <p class="mt-5 text-sm">No timestamp</p>
+          <p class="mt-5 text-base text-gray-500">No timestamp right now :(</p>
         </div>
       </div>
 
@@ -224,53 +224,26 @@ export default {
     <!-- End Card Section -->
   </div>
 
-  <!-- button refresh -->
-  <div class="fixed top-1/2 transform -translate-y-1/2 -right-1 z-10">
-    <a href="/">
-      <button
-        type="button"
-        class="opacity-50 py-3 px-3 inline-flex justify-center items-center gap-2 rounded-l-lg border border-transparent font-semibold bg-indigo-500 text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="18"
-          height="18"
-          fill="currentColor"
-          class="bi bi-arrow-clockwise"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"
-          />
-          <path
-            d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"
-          />
-        </svg>
-      </button>
-    </a>
-  </div>
-
   <!-- button actions -->
   <div class="fixed bottom-0 left-0 right-0 z-10 bg-white md:w-[420px] mx-auto">
-    <div class="flex">
+    <div class="flex p-4 gap-x-2">
       <button
         type="button"
-        class="py-3 px-4 inline-flex justify-center items-center gap-2 bg-blue-100 border border-transparent font-semibold text-blue-500 xl:hover:text-white xl:hover:bg-blue-500 focus:outline-none ring-offset-white focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm w-full"
+        class="py-3 px-4 inline-flex justify-center items-center gap-2 bg-blue-100 border border-transparent font-semibold text-blue-500 xl:hover:text-white xl:hover:bg-blue-500 focus:outline-none ring-offset-white focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm w-full shadow-md rounded-lg"
         v-on:click="handleStamp('milk')"
       >
         Milk
       </button>
       <button
         type="button"
-        class="py-6 px-4 inline-flex justify-center items-center gap-2 bg-purple-100 border border-transparent font-semibold text-purple-500 xl:hover:text-white xl:hover:bg-purple-500 focus:outline-none ring-offset-white focus:ring-purple-500 focus:ring-offset-2 transition-all text-sm w-full"
+        class="py-6 px-4 inline-flex justify-center items-center gap-2 bg-purple-100 border border-transparent font-semibold text-purple-500 xl:hover:text-white xl:hover:bg-purple-500 focus:outline-none ring-offset-white focus:ring-purple-500 focus:ring-offset-2 transition-all text-sm w-full shadow-md rounded-lg"
         v-on:click="handleStamp('pumping')"
       >
         Pumping
       </button>
       <button
         type="button"
-        class="py-3 px-4 inline-flex justify-center items-center gap-2 bg-red-100 border border-transparent font-semibold text-red-500 xl:hover:text-white xl:hover:bg-red-500 focus:outline-none ring-offset-white focus:ring-red-500 focus:ring-offset-2 transition-all text-sm w-full"
+        class="py-3 px-4 inline-flex justify-center items-center gap-2 bg-red-100 border border-transparent font-semibold text-red-500 xl:hover:text-white xl:hover:bg-red-500 focus:outline-none ring-offset-white focus:ring-red-500 focus:ring-offset-2 transition-all text-sm w-full shadow-md rounded-lg"
         v-on:click="handleStamp('diaper')"
       >
         Diaper
